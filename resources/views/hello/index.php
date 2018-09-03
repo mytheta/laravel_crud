180987a18255e7dc98acb873e6445f8cfbc79748
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-03 16:32:55 +0900

    [add]provider

---
+++ resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Hello/Index</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 { font-size:120pt; text-align:right; color:#fafafa; 
+margin:-50px 0px -120px 0px;}
+</style>
+</head>
+<body>
+<h1>Index</h1>
+<p>this is a sample page with php-template</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::get('hello','HelloController@index');
 
+
+// Route::get('/hello', function () {
+//     return view('hello.index');
+// });
